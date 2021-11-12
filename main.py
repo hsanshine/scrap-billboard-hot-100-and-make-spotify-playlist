767e0af841579e048909b2ba45911c5ca13a1030
Hamza Kyamanywa <sanshinehamza@gmail.com>
2021-11-12 23:33:38 +0900

    scrapping code added

---
+++ main.py
@@ -0,0 +1,28 @@
+# 2021-02-11
+#program to scrap billboard hot 100 of top songs of a day you enter then make sportify playlist from the list
+#by hamza
+
+import requests
+from bs4 import BeautifulSoup
+
+
+userDate= input('enter the date of songs in yyyy-mm-dd format : ')
+date = userDate
+billBoardUrl = f'https://www.billboard.com/charts/hot-100/{date}'
+
+selectLocation = 'li span'
+songClass = 'chart-element__information__song text--truncate color--primary'
+artistNameClass = 'chart-element__information__artist text--truncate color--secondary'
+print(billBoardUrl)
+response = requests.get(billBoardUrl)
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.title)
+songs = [ song.getText() for song in soup.find_all('span', attrs={'class': songClass})]
+print(len(songs))
+print(songs)
+artists = [artist.getText() for artist in soup.find_all('span', attrs={'class':artistNameClass})]
+print(len(artists))
+print(artists)
+
+
+
